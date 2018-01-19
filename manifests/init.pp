a2e499a58fba2d4b843b9b95f1980e9c1e5252b8
Chris Falck <chris.falck@tickboxconsulting.com>
2018-01-19 06:59:03 +0000

    debug

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,9 @@ application wordpress_app (
   String $db_user         = 'wordpress',
   String $db_pass         = 'wordpress',
   String $web_int         = '',
-  String $web_port        = '8090',
+  String $web_port        = '108090',
   String $lb_ipaddress    = '0.0.0.0',
-  String $lb_port         = '90',
+  String $lb_port         = '110090',
   String $lb_balance_mode = 'roundrobin',
   Array  $lb_options      = ['forwardfor','http-server-close','httplog'],
 ){
